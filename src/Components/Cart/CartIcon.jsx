9fb3ab6669a282f0bce11dc69a76f120c117ea3f
gojosataru001 <sharmahunny16197@gmail.com>
2025-07-21 14:21:09 +0530

    header section and cart icon

---
+++ src/Components/Cart/CartIcon.jsx
@@ -0,0 +1,16 @@
+// src/components/Cart/CartIcon.jsx
+const CartIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+      width="20"
+      height="20"
+    >
+      <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13l-1.5 6h13M10 21a1 1 0 100-2 1 1 0 000 2zm7 0a1 1 0 100-2 1 1 0 000 2z" />
+    </svg>
+  );
+};
+
+export default CartIcon;
---
+++ src/Components/Layout/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Fragment } from 'react'
+import mealsImage from "../../assets/meals.png"
+import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton'
+const Header = (props) => {
+  return (
+    <Fragment>
+        <header className={classes.header}>
+            <h1>React Meals</h1>
+            <HeaderCartButton/>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt=" A table full of delicious meal" />
+        </div>
+
+    </Fragment>
+  )
+}
+
+export default Header
